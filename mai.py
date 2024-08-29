4de94fff18a5d130c217af411a531a3d0eda718d
Terence Laminger <laminter001@warren.k12.in.us>
2024-08-29 08:10:25 -0400

    go

---
+++ mai.py
@@ -0,0 +1,14 @@
+F_text = input("Enter the temp in degrees F: ")
+F = float(F_text)
+C = 5 * (F - 32) / 9
+print("The temp in degrees C is", C)
+
+C_text = input("Enter the temp in degrees C: ")
+input("C_text in C")
+input("1.8 * C + 32")
+print("The temp in degrees F is", F)
+#INSTRUCTION: you do the rest.  Find the temperature in degrees F
+#This instruction should be 3 lines of code
+#Line 7 should be turning C_text into C with a casting function
+#line 8 should be calculating F using 1.8 * C + 32
+#line 9 should be printing the result like I did in line 4
